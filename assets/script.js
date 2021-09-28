ca55526264990fe5e1ad5520934aa8814d638779
Sam Kilpatrick <samkilpatrick903@gmail.com>
2021-09-27 21:41:32 -0500

    Working on js

---
+++ assets/script.js
@@ -0,0 +1,41 @@
+
+const content = document.querySelector('#content');
+
+// Questions
+
+q1 = {
+    index: 0,
+    title: "What year did the Grateful Dead form?",
+    choices: ['1965', '1970', '1969', '1975'],
+    answer: '1965',
+}
+
+q2 = {
+    index: 1,
+    title: "Who wqas the lead guitarist for the Grateful Dead?",
+    choices: ['Jerry Garcia', 'Phil Lesh', 'Bill Kreutzman', 'Brent Mydland'],
+    answer: 'Jerry Garcia',
+}
+
+q3 = {
+    index: 2,
+    title: "What state did Jack Straw hail from?",
+    choices: ['Texas', 'Kansas', 'Louisiana', 'California'],
+    answer: 'Kansas',
+}
+
+q4 = {
+    index: 3,
+    title: "Wake up to find that your are the ____ of the world",
+    choices: ['hands', 'base', 'eyes', 'weight'],
+    answer: 'bamboo',
+}
+
+q5 = {
+    index: 4,
+    title: "What year was the final Grateful Dead show with the full lineup?",
+    choices: ['2001', '1995', '1989', '1979'],
+    answer: '1989'
+}
+
+
