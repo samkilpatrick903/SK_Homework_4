85482a0ed6570d26bd87849aa1deccfe21abfecf
Sam Kilpatrick <samkilpatrick903@gmail.com>
2021-09-28 22:00:46 -0500

    Started on css and added more js

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# SK_Homework_4
+# SK_Homework_4 Grateful Dead Quiz
+
+Description
+Assigned to create a timed trivia quiz with multiple choice answers. The app runs dynamically updated HTML and CSS powered by Javascript
+
+Links
+
+
+*WILL COME BACK TO THIS ASSIGNMENT*
--- assets/script.js
+++ assets/script.js
@@ -38,4 +38,132 @@ q5 = {
     answer: '1989'
 }
 
+const questions = [q1, q2, q3, q4, q5];
+score = 0;
+let index = 0; 
 
+content.addEventListener('click', function(e) {
+    if(e.target.id === "start-button" ) {
+        runQuiz();
+    }
+    if(e.target.classList.contains('question-button')){
+        let parent = e.target.parentElement; 
+        let p = parent.querySelector('p');
+        let id = e.target.parentElement.getAttribute('data-index');
+        if(e.target.innerHTML === questions[id].correctAnswer){
+            p.innerHTML = 'you got the question right!';
+            score += 20; 
+            questionAdvance(id)
+        } else {
+            p.innerHTML = 'you got the question wrong!';      
+            questionAdvance(id)
+        }
+    } 
+})
+
+function init() {
+    
+    generateHome();
+
+};
+
+init();
+
+//Home Screen
+
+function generateHome() {
+
+    html = `
+    <div id="home">
+        <h1>Grateful Dead Quiz</h1>
+        <p>What do you know about the Grateful Dead?!</p>
+        <button id="start-button">Start Quiz</button>
+    </div>
+    `;
+
+    content.innerHTML += html; 
+}
+
+
+//Quiz
+
+function runQuiz() {
+    
+    constructQuiz();
+
+}
+
+function constructQuiz()  {
+    content.innerHTML = '';
+
+    for(let i = 0; i < questions.length; i++) {
+        
+        html = `
+        
+        <div data-display="hidden" data-index="${questions[i].index}" class="question">
+            <h2>${questions[i].title}</h2>
+            <button class="question-button">${questions[i].answers[0]}</button>
+            <button class="question-button">${questions[i].answers[1]}</button>
+            <button class="question-button">${questions[i].answers[2]}</button>
+            <button class="question-button">${questions[i].answers[3]}</button>
+            <p></p>
+        </div>
+        `;
+    
+        content.innerHTML += html;  
+        
+        const p = content.querySelector('p');
+        const div = content.querySelectorAll('div');
+
+        firstQuestion = content.querySelector('div');
+        firstQuestion.setAttribute("data-display", "visible")
+ 
+    }
+}
+
+function questionAdvance(id){
+    if(id === '4'){
+        setTimeout(function(){
+            showScore();
+        }, 1000);
+    } else { 
+        setTimeout(function() {
+            const questionsdev = content.querySelectorAll('.question');  
+            let currentQuestion = questionsdev[index];
+            currentQuestion.setAttribute('data-display', "hidden");
+            index += 1;
+            currentQuestion = questionsdev[index];
+            currentQuestion.setAttribute('data-display', "visible");
+        }, 1000);
+    }
+}
+
+function showScore(){
+
+    html = `
+        
+        <div class="score-board">
+            <h2>All Done!</h2>
+            <p>Your final score was ${score}.</p>
+            <form>
+                <label>Enter Intials:</label>
+                <input type="text">
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+        `;
+        content.innerHTML = html;  
+};
+
+startBtn.addEventListener("click", startQuiz);
+
+// Present Questions
+
+function startQuiz() {
+   timerInterval = setInterval(clockTick, 1000);
+    startScreen.setAttribute("class", "hide");
+        questionBox.removeAttribute("class");
+        choices.removeAttribute("class")
+        getQuestions();
+}
+ 
